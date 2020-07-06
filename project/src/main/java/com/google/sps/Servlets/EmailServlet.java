7951fece5de81817b9926e1ff62351592a6cb22c
Max Friedman <max.friedman0@gmail.com>
2020-07-06 21:22:34 +0000

    Add support for emailing invitations

--- project/src/main/java/com/google/sps/Servlets/EmailServlet.java
+++ project/src/main/java/com/google/sps/Servlets/EmailServlet.java
@@ -13,11 +13,51 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import com.google.sps.models.Database;
+import com.google.sps.models.User;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
+
 
 @WebServlet("/Email")
 public class EmailServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    long userID = (long) request.getSession(false).getAttribute("userID");
+    User user = Database.getUserByID(userID);
+    
+    String toEmail = request.getParameter("email");
+    String fromEmail = user.getEmail();
+    String subject = "Invitation to edit document on Collaborative Code Editor";
+    String message = "Hi! I have invited you to join my document on Collaborative Code Editor, go to xyz.com to sign up and view!";
 
+    sendEmail(toEmail, fromEmail, subject, message);
+
+  }
+
+  private void sendEmail(String to, String from, String subject, String message){
+    Properties props = new Properties();
+    Session session = Session.getDefaultInstance(props, null);
+
+    try {
+      Message msg = new MimeMessage(session);
+      msg.setFrom(new InternetAddress(from, "Example.com Admin"));
+      msg.addRecipient(Message.RecipientType.TO, new InternetAddress(to, "Mr. User"));
+      msg.setSubject(subject);
+      msg.setText(message);
+      Transport.send(msg);
+    } catch (AddressException e) {
+      // ...
+    } catch (MessagingException e) {
+      // ...
+    } catch (UnsupportedEncodingException e) {
+      // ...
+    }
   }
 }
