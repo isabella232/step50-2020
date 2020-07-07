d37a8b3284b7eaf6d5893d201429170132c9bb0d
Grace Luo <graceluo@berkeley.edu>
2020-07-07 10:02:42 -0700

    Nav Panel Form Improvements (#35)
    * submit button is disabled until all fields are filled out (the input and the dropdown)
    * input placeholder disappears on focus / click + resets on page reload
    * dropdown collapses on selection
    * dropdown prop selectedItem renamed to value, so syntax like dropdown.value now works
    * documents open in new tab

--- project/src/main/webapp/components/dropdown-element.js
+++ project/src/main/webapp/components/dropdown-element.js
@@ -7,6 +7,12 @@ import {html, LitElement} from 'https://unpkg.com/@polymer/lit-element/lit-eleme
  * changeLabel - whether to change the top label based on selection
  * styling - css classes to apply to all elements in the dropdown.
  * otherwise let the dropdown inherit styling via css.
+ *
+ * Usage
+ * const dropdown = document.querySelector('dropdown-element');
+ * const value = dropdown.value;
+ * all attributes under the properties() getter can also be 
+ * access as dropdown.[attribute-name]
  */
 export class DropdownElement extends LitElement {
   static get properties() {
@@ -14,10 +20,9 @@ export class DropdownElement extends LitElement {
       options: {type: Array},
       name: {type: String},
       label: {type: String},
-      selectedItem: {type: String},
+      value: {type: String},
       changeLabel: {type: Boolean},
       showDropdown: {type: String},
-      selectedItem: {type: String},
       styling: {type: String},
     };
   }
@@ -27,7 +32,7 @@ export class DropdownElement extends LitElement {
     this.options = [];
     this.name = '';
     this.label = '';
-    this.selectedItem = '';
+    this.value = '';
     this.changeLabel = true;
     this.showDropdown = false;
     this.styling = '';
@@ -42,36 +47,42 @@ export class DropdownElement extends LitElement {
     this.showDropdown = !this.showDropdown;
   }
 
-  toggleSelectedItem(item) {
-    this.selectedItem = item;
+  togglevalue(item) {
+    this.value = item;
+    this.toggleDropdown();
+    this.createChangeEvent();
   }
+
+  createChangeEvent() {
+    let event = new Event('change');
+    this.dispatchEvent(event);
+  }
+  
 
   render() {
     let dropdownState = this.showDropdown ? 'is-active' : '';
-    let dropdownLabel = (this.changeLabel && this.selectedItem != '') ?
-        this.selectedItem :
-        this.label;
+    let dropdownLabel = 
+      (this.changeLabel && this.value != '') ? this.value : this.label;
     return html`        
       <div>
         <!-- Add hidden input to transmit GET request data -->
-        <input type="hidden" name=${this.name} value=${this.selectedItem}>
+        <input name=${this.name} id=${this.name} type="hidden" value=${this.value}>
         <div class=${'dropdown ' + dropdownState + ' ' + this.styling}>
           <div class=${'dropdown-trigger ' + this.styling}>
-            <button type="button" class=${this.styling} @click=${
-        this.toggleDropdown} aria-haspopup="true" aria-controls="dropdown-menu">
+            <button type="button" class=${this.styling} @click=${this.toggleDropdown} 
+              aria-haspopup="true" aria-controls="dropdown-menu">
               <span>${dropdownLabel}</span>
               <span class="icon is-small">
                 <i class="fa fa-angle-down" aria-hidden="true"></i>
               </span>
             </button>
           </div>
-          <div class="${
-                           'dropdown-menu ' +
-        this.styling}" id="dropdown-menu" role="menu">
+          <div class="${'dropdown-menu ' + this.styling}" 
+            id="dropdown-menu" role="menu">
             <div class="dropdown-content">
               ${this.options.map((option) => html`
                   <a href="#" 
-                    @click=${() => this.toggleSelectedItem(option)} 
+                    @click=${() => this.togglevalue(option)} 
                     class="dropdown-item"> 
                     ${option} 
                   </a>
