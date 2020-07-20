0c51f3887a0568be291bbd59c8f00b1ef3b37ba4
Valentino <tinodore@gmail.com>
2020-07-20 18:19:24 +0000

    Add comment servlet

---
+++ project/src/main/java/com/google/sps/Servlets/CommentServlet.java
@@ -0,0 +1,27 @@
+package com.google.sps.servlets;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.gson.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Comment")
+public class CommentServlet extends HttpServlet {
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+
+    long userID = (long) request.getSession(false).getAttribute("userID");
+    String commentData = request.getParameter("commentData");
+    long commentID = Database.createComment(userID, commentData);
+
+    out.println(commentID);
+  }
+}
