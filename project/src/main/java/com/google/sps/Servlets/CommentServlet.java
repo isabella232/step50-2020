48882714f0038458e69425419aae328d94dbaba4
tino-ai <61718297+tino-ai@users.noreply.github.com>
2020-07-22 14:52:34 -0400

    Update CommentServlet.java

--- project/src/main/java/com/google/sps/Servlets/CommentServlet.java
+++ project/src/main/java/com/google/sps/Servlets/CommentServlet.java
@@ -12,34 +12,30 @@ import java.text.SimpleDateFormat;
 import com.google.sps.models.Database;
 import com.google.sps.models.Comment;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 @WebServlet("/Comment")
+@MultipartConfig
 public class CommentServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html");
     PrintWriter out = response.getWriter();
 
-    System.out.println(request.getParameter("commentData"));
     System.out.println(request.getParameter("commentDate"));
+    System.out.println(request.getParameter("commentData"));
 
     long userID = (long) request.getSession(false).getAttribute("userID");
     String commentData = request.getParameter("commentData");
+
     String date = request.getParameter("commentDate");
 
     long commentID = Database.createComment(userID, commentData, date);
-
-    if ("POST".equalsIgnoreCase(request.getMethod())) 
-    {
-      System.out.println(request.getReader().lines().collect(Collectors.joining(System.lineSeparator())));
-    }
-
-
-    System.out.println(request.getParameterNames());
 
     out.println(commentID);
   }
