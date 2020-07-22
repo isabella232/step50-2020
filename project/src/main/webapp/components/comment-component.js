3e5053a7201d5b877d62ffdd0da4e4274a3f9342
Max Friedman <max.friedman0@gmail.com>
2020-07-22 21:33:37 +0000

    Add functionality to render prexisting comments

--- project/src/main/webapp/components/comment-component.js
+++ project/src/main/webapp/components/comment-component.js
@@ -7,7 +7,10 @@ export class CommentComponent extends LitElement {
       codeMirror: {type: Object},
       placeholder : {type: String},
       name : {type: String},
-      date : {type: String}
+      date : {type: String},
+      hash : {type: String},
+      text : {type: String},
+      exists : {type: Boolean}
     };
   }
 
@@ -16,6 +19,9 @@ export class CommentComponent extends LitElement {
     this.placeholder = 'Write a comment...';
     this.name ='';
     this.date = '';
+    this.hash = '';
+    this.text = '';
+    this.exists = false;
   }
 
   // Remove shadow DOM so styles are inherited
@@ -23,33 +29,26 @@ export class CommentComponent extends LitElement {
     return this;
   }
 
-  makeDate() {
-    this.date = new Intl.DateTimeFormat('en-US', {
-      month: 'long',
-      day: 'numeric',
-      hour: 'numeric',
-      minute: 'numeric'
-    }).format(new Date());
-  }
-
   render() {
     return html` 
       <form class="comment-group" id="comment-form" onsubmit="return subComment()">
         <div class="comment-div">
-        ${this.makeDate()}
           <p class="comment-name">${this.name}</p>
           <p class="comment-date">${this.date}</p>
           <input type="hidden" id="commentDate" name="commentDate" value="${this.date}">
+          <input type="hidden" id="documentHash" name="documentHash" value="${this.hash}">
           <div class="comment-delete">
             <button class="delete"></button>
           </div>
-          <input type="textarea"
+          ${this.exists ? html`<p class ="comment-text">${this.text}</p>`:
+            html`<input type="textarea"
             name="commentData" id="commentData"
             class="comment-txt"
             placeholder=${this.placeholder}
             autocomplete="off" 
           ></input>
-          <input type="submit">Submit</input>
+          <input type="submit">Submit</input>`
+          }
         </div>
       </form>
     `;
